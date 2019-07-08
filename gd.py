e834dada6d081b4c33668894e56b922a694a18cd
dikam76 <dkamzolov@yandex.ru>
2019-07-09 00:30:50 +0300

    Update gd.py

--- gd.py
+++ gd.py
@@ -3,10 +3,6 @@ import numpy as np
 def gradient_step(x_k, grad, step):
     return x_k - step * grad(x_k)
 
-def newton_step(x_k, grad, hessian):
+def newton_step(x_k, grad, hessian, step = 1):
     H = np.linalg.inv(hessian)
-    return x_k - H @ grad(x_k)
-
-def demph_newton_step(x_k, grad, hessian, step):
-    H = np.linalg.inv(hessian)
-    return x_k - step * H @ grad(x_k) 
+    return x_k - step * H @ grad(x_k)
